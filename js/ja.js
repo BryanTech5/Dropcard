e9f2544e98cfd194318c429e6c3420d398484b6b
saviour john <bryansub@gmail.com>
2024-08-27 12:45:18 +0100

    saviour initial commit

---
+++ js/ja.js
@@ -0,0 +1,71 @@
+// alert("Welcome to BryanTech")
+// var name = prompt("What is your name?")
+// var age = prompt("What is your age?")
+// var email = prompt("Email Adress")
+// var phone = prompt("Phone number")
+// var confirm = confirm("Thank you for your interest in our event!!!")
+
+// console.log(name)
+// console.log(age)
+// console.log(email)
+// console.log(phone)
+// console.log(confirm)
+
+
+
+// let admin;
+// let name;
+// name = "john";
+// admin = name
+// alert(admin)
+
+// let ourPlanet = "Earth"
+
+// let currentUser = "Saviour"
+
+// let doubleQuote = "Double Quote"
+
+// let singleQuote = 'Single Quote'
+
+// let backtick = `Back Tick`
+
+
+// const firstName = "Saviour";
+
+// const surName = "john";
+
+// const fullName = `Hello ${firstName} ${surName}`;
+
+// console.log(fullName);
+
+// alert(fullName)
+
+// let n = 2
+// n += 1
+// alert(n) 
+
+// let a = prompt('name')
+
+// let b = prompt('number')
+// alert(a + "  " + b)
+
+// let n = 6
+// n--
+// alert(n)
+
+// let a = 1, b =1;
+
+// let c = ++a;
+
+// let d = b++;
+// alert(d)
+
+// alert(1 === 1)
+
+// let message = prompt("what is your name?")
+// console.log(message)
+// alert(`Good morning, ${message}`)
+
+//  const userInput = prompt("Calculate:");
+//  const result = eval(userInput);
+//  alert("Result:" + result);
---
+++ js/script.js
@@ -0,0 +1,32 @@
+ //prompt("what is ur name")
+// alert("talk to me")
+// confirm("cool?")
+// console.log("hey")
+
+//  var name= "saviour"
+ // var name = prompt("What is ypur name?")
+
+  // let name =("What is your name?")
+  
+  // const name =("What is your name?")
+  // console.log(name)
+
+  // var age = 20
+
+
+
+  // string data type
+  // let firstname =("Saviour")
+  // let lastname =("afa")
+  // let middlename =("j")
+
+  // Number data type
+  // let age = 20
+  // let amount = 300
+  // let population = 3000000
+
+  //null data type
+  // let country = null
+
+  //boolean data type
+  // let active = false
